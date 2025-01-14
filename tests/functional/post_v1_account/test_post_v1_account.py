b2438b2f06a1c2320213e55b5b76e7e81fba77b1
antongolovan <golovan.ant@gmail.com>
2025-01-14 18:27:16 +0300

    добавил обработку при сериализации json в методе get_activation_token_by_login

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,8 @@
-from json import loads
+import pprint
+from json import (
+    loads,
+    JSONDecodeError,
+)
 
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
@@ -10,7 +14,7 @@ def test_post_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'golovan13'
+    login = 'golovan20'
     password = '112233'
     email = f'{login}@mail.ru'
 
@@ -40,7 +44,7 @@ def test_post_v1_account():
 
 
     # Получить токен из почтового сервиса
-    token = get_activation_token_by_login(login,response)
+    token = get_activation_token_by_login(login, response)
 
     assert token is not None, f'Токен для пользователя {login} не был получен'
 
@@ -51,7 +55,6 @@ def test_post_v1_account():
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, 'Пользователь не был активирован'
-
 
     # Авторизация пользователя
 
@@ -67,15 +70,19 @@ def test_post_v1_account():
     print(response.text)
     assert response.status_code == 200, 'Пользователь не был авторизован'
 
-def get_activation_token_by_login(login, response):
+
+def get_activation_token_by_login(
+        login,
+        response
+):
     token = None
     for item in response.json()['items']:
-        user_data = loads(
-            item['Content']['Body']
-            )
+        try:
+            user_data = loads(item['Content']['Body'])
+            pprint.pprint(user_data)
+        except (JSONDecodeError, KeyError):
+            continue
         user_login = user_data['Login']
         if user_login == login:
-            token = user_data['ConfirmationLinkUrl'].split(
-                '/'
-                )[-1]
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
     return token
