81c2406ada090ea0532d46d10bdd5e3390fd2915
antongolovan <golovan.ant@gmail.com>
2025-01-14 13:14:23 +0300

    вынесли функции в классы клиенты

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,10 +1,8 @@
-import requests
-import pprint
 from json import loads
 
-from account_api import AccountApi
-from login_api import LoginApi
-from mailhog_api import MailhogApi
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from api_mailhog.apis.mailhog_api import MailhogApi
 
 def test_post_v1_account():
     # Регистрация пользователя
@@ -12,7 +10,7 @@ def test_post_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'golovan10'
+    login = 'golovan13'
     password = '112233'
     email = f'{login}@mail.ru'
 
