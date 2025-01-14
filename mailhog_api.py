90860481c929170984491a688259cde5b071fa40
antongolovan <golovan.ant@gmail.com>
2025-01-14 11:26:56 +0300

    вынесли функции в классы клиенты

---
+++ mailhog_api.py
@@ -0,0 +1,34 @@
+from resource import RLIMIT_CPU
+
+import requests
+
+
+class MailhogApi:
+
+    def __init__(
+            self,
+            host,
+            headers=None
+    ):
+        self.host = host
+        self.email = headers
+
+    def get_api_v2_messages(
+            self,
+            limit=50
+    ):
+        """
+         Get Users emails
+        :param limit:
+        :return:
+        """
+        params = {
+            'limit': limit
+        }
+
+        response = requests.get(
+            url=f'{self.host}/api/v2/messages',
+            params=params,
+            verify=False
+        )
+        return response
--- tests/post_v1_account/test_post_v1_account.py
+++ tests/post_v1_account/test_post_v1_account.py
@@ -2,10 +2,17 @@ import requests
 import pprint
 from json import loads
 
+from account_api import AccountApi
+from login_api import LoginApi
+from mailhog_api import MailhogApi
+
 def test_post_v1_account():
     # Регистрация пользователя
+    account_api= AccountApi(host='http://5.63.153.31:5051')
+    login_api = LoginApi(host='http://5.63.153.31:5051')
+    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'golovan6'
+    login = 'golovan10'
     password = '112233'
     email = f'{login}@mail.ru'
 
@@ -15,10 +22,7 @@ def test_post_v1_account():
         'password': password,
     }
 
-    response = requests.post(
-        'http://5.63.153.31:5051/v1/account',
-        json=json_data
-    )
+    response = account_api.post_v1_account(json_data=json_data)
 
     print(response.status_code)
     print(response.text)
@@ -30,11 +34,7 @@ def test_post_v1_account():
         'limit': '50',
     }
 
-    response = requests.get(
-        'http://5.63.153.31:5025/api/v2/messages',
-        params=params,
-        verify=False
-    )
+    response = mailhog_api.get_api_v2_messages()
 
     print(response.status_code)
     print(response.text)
@@ -42,25 +42,13 @@ def test_post_v1_account():
 
 
     # Получить токен из почтового сервиса
-    token = None
-    for item in response.json()['items']:
-        user_data = loads(item['Content']['Body'])
-        user_login = user_data['Login']
-        if user_login == login:
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+    token = get_activation_token_by_login(login,response)
 
     assert token is not None, f'Токен для пользователя {login} не был получен'
 
     # Активация пользователя
 
-    headers = {
-        'accept': 'text/plain',
-    }
-
-    response = requests.put(
-        f'http://5.63.153.31:5051/v1/account/{token}',
-        headers=headers
-    )
+    response = account_api.put_v1_account_token(token=token)
 
     print(response.status_code)
     print(response.text)
@@ -75,11 +63,21 @@ def test_post_v1_account():
         'rememberMe': True,
     }
 
-    response = requests.post(
-        'http://5.63.153.31:5051/v1/account/login',
-        json=json_data
-    )
+    response = login_api.post_v1_account_login(json_data=json_data)
 
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, 'Пользователь не был авторизован'
+
+def get_activation_token_by_login(login, response):
+    token = None
+    for item in response.json()['items']:
+        user_data = loads(
+            item['Content']['Body']
+            )
+        user_login = user_data['Login']
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split(
+                '/'
+                )[-1]
+    return token
