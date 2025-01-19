14160032785d5dff60d7a148520439596a00408d
antongolovan <golovan.ant@gmail.com>
2025-01-19 20:27:13 +0300

    добавил класс конфигурации логгов

--- restclient/client.py
+++ restclient/client.py
@@ -4,11 +4,18 @@ import structlog
 import uuid
 import curlify
 
+from restclient.configuration import Configuration
+
+
 class RestClient:
 
-     def __init__(self, host, headers=None):
-         self.host = host
-         self.headers = headers
+     def __init__(
+             self,
+            configuration: Configuration
+     ):
+         self.host = configuration.host
+         self.headers = configuration.headers
+         self.disable_log = configuration.disable_log
          self.session = session()
          self.log = structlog.getLogger(__name__).bind(service='api')
 
@@ -43,6 +50,10 @@ class RestClient:
      def _send_request(self, method, path, **kwargs):
          log = self.log.bind(event_id=str(uuid.uuid4()))
          full_url = self.host + path
+
+         if self.disable_log:
+             rest_responce = self.session.request(method=method, url=full_url, **kwargs)
+             return rest_responce
 
          log.msg(
              event='Request',
