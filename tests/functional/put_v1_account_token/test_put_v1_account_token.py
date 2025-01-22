d2f402f1b597753340ebba4d536e8d199b8efcbd
antongolovan <golovan.ant@gmail.com>
2025-01-22 17:41:49 +0300

    Фикс в тестовых файлах Добавил уникальные логины пользователей Убрал триггер на push в джобе

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -1,19 +1,35 @@
 import time
 
-from api_mailhog.apis.mailhog_api import MailhogApi
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
+from api_mailhog.apis.mailhog_api import MailhogApi
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
+import structlog
+
 from tests.functional.post_v1_account.test_post_v1_account import get_activation_token_by_login
 
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            # sort_keys=True
+        )
+    ]
+)
 
 def test_put_v1_account_token():
 
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mailhog_api = MailhogApi(configuration=mailhog_configuration)
 
     # Регистрация пользователя
-    login = 'golovan119'
+    login = 'golovan124'
     password = '112233'
     email = f'{login}@mail.ru'
 
