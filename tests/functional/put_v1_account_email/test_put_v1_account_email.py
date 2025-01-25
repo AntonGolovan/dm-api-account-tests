3c73b51ab8099542f1393bf920c7b754a76ecb10
antongolovan <golovan.ant@gmail.com>
2025-01-25 17:44:36 +0300

    Добвляю в ПР уникальные логины пользователей

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -24,7 +24,7 @@ def test_put_v1_account_email():
     mailhog = MailHogApi(configuration=mailhog_configuration)
 
     account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
-    login = 'golovan08'
+    login = 'golovan013'
     password = '112233'
     email = f'{login}@mail.ru'
 
